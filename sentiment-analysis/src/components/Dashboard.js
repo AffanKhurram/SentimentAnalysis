8b8c1c2c4b0822d800cf365cabc12a76b6b8e424
Affan Khurram <affankhurram1@gmail.com>
2021-11-17 12:15:19 -0600

    basic css, buttons don't work yet

--- sentiment-analysis/src/components/Dashboard.js
+++ sentiment-analysis/src/components/Dashboard.js
@@ -9,9 +9,11 @@
 
 
 import React, { useEffect } from 'react'
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import { useNavigate } from 'react-router'
+import './Dashboard.css'
 
 export default function Dashboard() {
     const { currentUser } = useAuth()
@@ -28,8 +30,13 @@ export default function Dashboard() {
     // Return the layout of the page
     return (
         <div>
-            <h1>Dashboard</h1>
-            <Link to="/login">Login</Link><br></br>
+            <h1 className="header">Review Busters</h1>
+            <Link to="/login" className="Button">
+                <Button>
+                    Login
+                </Button>    
+            </Link><br></br>
+            
             <Link to="/signup">Sign Up</Link><br></br>
             <Link to="/wordprocessing">Word Processing Page</Link>
         </div>
