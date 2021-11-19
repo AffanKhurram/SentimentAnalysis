f15b7c0314d5c3504b99ef639d0540ac4e3878d2
BuildTools <unconfigured@null.spigotmc.org>
2021-11-18 19:47:35 -0600

    changes

--- sentiment-analysis/src/components/Page2.js
+++ sentiment-analysis/src/components/Page2.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Page2.css'
+import './Dashboard.css'
+import { Link } from 'react-router-dom'
 
 export default function Page2() {
     return (
         <div>
-            <h1>Settings</h1>
+            <h1 className = "headers">Settings</h1><br></br>
+            <br></br>
+            <br></br>
+            <h3> Change the Back Ground Color </h3>
+            <h3 classname = "headers"><Link to="/">Dashboard</Link></h3>
+
         </div>
     )
 }
---
+++ sentiment-analysis/src/components/Page2code.js
@@ -0,0 +1,23 @@
+
+var square = document.getElementById("square"),
+    clickMe = document.getElementById('clickMe'); //Keeping it unobstrusive
+function doDemo () {
+
+  var button = this;
+  var allElements = document.getElementsByTagName("*");
+for (var i = 0, len = allElements.length; i < len; i++) {
+    var element = allElements[i];
+    // element.style.border = ...
+}
+  square.style.backgroundColor = "#fa4";
+  button.setAttribute("disabled", "true");
+  setTimeout(clearDemo, 2000, button);
+}
+
+function clearDemo (button) {
+  var square = document.getElementById("square");
+  square.style.backgroundColor = "transparent";
+  button.removeAttribute("disabled");
+}
+
+clickMe.onclick = doDemo; //Onclick call. Pass no arguments.​​​​​
